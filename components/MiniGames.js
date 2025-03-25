1a9f9f52da61e2af4adfa79729d67b3dc010d5a2
creatthread <155624498+creatthread@users.noreply.github.com>
2025-03-25 13:03:46 +0800

    Update MiniGames.js

--- components/MiniGames.js
+++ components/MiniGames.js
@@ -1,13 +1,10 @@
 export default function MiniGames() {
-    return (
-      <div>
-        <iframe 
-          src="/games/oracle-match/index.html"  // 确保 HTML 文件路径正确
-          width="1000px"
-          height="600px"
-          style={{ border: "none" }}
-        ></iframe>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <iframe 
+        src="/games/oracle-match/index.html"  // 确保 HTML 文件路径正确
+        style={{ border: "none" }}
+      ></iframe>
+    </div>
+  );
+}
